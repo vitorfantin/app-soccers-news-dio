894966b61014f76f4c9e31191baf48b82a232d19
Vitor Fantin <fantinfx@gmail.com>
2022-07-10 21:09:47 -0300

    confirmação de alterações cores + arredondamento dos botões

---
+++ app/build/generated/source/buildConfig/debug/me/dio/soccernews/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package me.dio.soccernews;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "me.dio.soccernews";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
